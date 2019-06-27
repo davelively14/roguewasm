782a957aff2773ab46b09cf7cb505ebb6322a96c
davelively14 <dave.lively@salesloft.com>
2019-06-27 09:16:44 -0400

    Hello worlded

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Import 'window.alert'
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+// Export a 'hello' function
+#[wasm_bindgen]
+pub fn hello(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
